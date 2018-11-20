40418beebc7c7b8f5449fcf3dc5a3d91d3583ad4
Jitendra <itsji10dra@gmail.com>
2018-11-20 19:56:03 +0530

    Added cache cleanup...

--- FireCache.swift
+++ FireCache.swift
@@ -14,6 +14,14 @@ public class FireCache<T: Cacheable> {
     
     private let memoryCache = NSCache<NSString, AnyObject>()
     
+    private lazy var lastAccessed: [String: Date] = [:]
+
+    private var expiryCheckTimer: Timer!
+
+    public var expiryCheckTimeInterval: TimeInterval = 30
+
+    public var cacheLifeSpan: TimeInterval = 60
+
     // MARK: - Initializer
     
     public init() {
@@ -27,11 +35,18 @@ public class FireCache<T: Cacheable> {
                                                selector: #selector(clearMemoryCache),
                                                name: UIApplication.didReceiveMemoryWarningNotification,
                                                object: nil)
+
+        expiryCheckTimer = Timer.scheduledTimer(withTimeInterval: expiryCheckTimeInterval,
+                                                repeats: true,
+                                                block: { [weak self] _ in
+                                                    self?.clearExpiredCache()
+        })
     }
     
     // MARK: - DeInitializer
     
     deinit {
+        expiryCheckTimer.invalidate()
         NotificationCenter.default.removeObserver(self)
     }
     
@@ -40,20 +55,28 @@ public class FireCache<T: Cacheable> {
     public func store(_ object: T,
                       forKey key: String,
                       completionHandler: (() -> Void)? = nil) {
-        
         memoryCache.setObject(object as AnyObject, forKey: key as NSString)
+        lastAccessed[key] = Date()
         completionHandler?()
     }
     
-    public func removeImage(forKey key: String,
-                            completionHandler: (() -> Void)? = nil) {
-        
+    public func remove(forKey key: String,
+                       completionHandler: (() -> Void)? = nil) {
         memoryCache.removeObject(forKey: key as NSString)
         completionHandler?()
     }
     
     public func retrieve(forKey key: String) -> T? {
+        lastAccessed[key] = Date()
         return memoryCache.object(forKey: key as NSString) as? T
+    }
+    
+    public func clearExpiredCache() {
+        lastAccessed.forEach { (key, access) in
+            if Date().timeIntervalSince(access) > cacheLifeSpan {
+                remove(forKey: key)
+            }
+        }
     }
     
     @objc
