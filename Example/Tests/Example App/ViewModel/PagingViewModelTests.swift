a531b5e60d3d0029a1cbc5af51b8c4d22835c1d4
Jitendra <itsji10dra@gmail.com>
2018-11-22 00:49:41 +0530

    Updated Paging view model tests...

--- Example/Tests/Example App/ViewModel/PagingViewModelTests.swift
+++ Example/Tests/Example App/ViewModel/PagingViewModelTests.swift
@@ -25,18 +25,33 @@ class PagingViewModelTests: XCTestCase {
                                              color: UIColor(hexString: $0.color) ) }
         })
 
-        let expectation = self.expectation(description: "Paging Data Fetching")
+        let expectationPage0 = self.expectation(description: "Paging Data Fetching - Page 0")
 
         pagingViewModel.loadMoreData { (data, error, page) in
             XCTAssertNotNil(data)
             XCTAssertNil(error)
             XCTAssertEqual(page, 0)
-            expectation.fulfill()
+            expectationPage0.fulfill()
         }
         
         self.waitForExpectations(timeout: 5) { error in
             if let error = error {
-                XCTFail("Paging Data Fetching waitForExpectationsWithTimeout errored: \(error)")
+                XCTFail("Paging Data Fetching - Page 0 - Expectations Timeout errored: \(error)")
+            }
+        }
+        
+        let expectationPage1 = self.expectation(description: "Paging Data Fetching - Page 1")
+        
+        pagingViewModel.loadMoreData { (data, error, page) in
+            XCTAssertNotNil(data)
+            XCTAssertNil(error)
+            XCTAssertEqual(page, 1)
+            expectationPage1.fulfill()
+        }
+        
+        self.waitForExpectations(timeout: 5) { error in
+            if let error = error {
+                XCTFail("Paging Data Fetching - Page 1 - Expectations Timeout errored: \(error)")
             }
         }
     }
