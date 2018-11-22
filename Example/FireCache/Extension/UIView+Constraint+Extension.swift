345d29776c104343b7239ac92d2de2b5b68ff58a
Jitendra <itsji10dra@gmail.com>
2018-11-22 21:48:54 +0530

    Removed unused methods...

--- Example/FireCache/Extension/UIView+Constraint+Extension.swift
+++ Example/FireCache/Extension/UIView+Constraint+Extension.swift
@@ -28,28 +28,6 @@ extension UIView {
     }
     
     @discardableResult
-    public func alignLessThanOrEqualTo(_ view: UIView, padding inset: UIEdgeInsets = .zero, activate: Bool = true) -> ConstraintInfo {
-        translatesAutoresizingMaskIntoConstraints = false
-        let constraint: ConstraintInfo = [.top: topAnchor.constraint(lessThanOrEqualTo: view.topAnchor, constant: inset.top),
-                                          .bottom: bottomAnchor.constraint(lessThanOrEqualTo: view.bottomAnchor, constant: inset.bottom),
-                                          .leading: leadingAnchor.constraint(lessThanOrEqualTo: view.leadingAnchor, constant: inset.left),
-                                          .trailing: trailingAnchor.constraint(lessThanOrEqualTo: view.trailingAnchor, constant: inset.right)]
-        defer { if activate { NSLayoutConstraint.activate(Array(constraint.values)) } }
-        return constraint
-    }
-    
-    @discardableResult
-    public func alignGreaterThanOrEqualTo(_ view: UIView, padding inset: UIEdgeInsets = .zero, activate: Bool = true) -> ConstraintInfo {
-        translatesAutoresizingMaskIntoConstraints = false
-        let constraint: ConstraintInfo = [.top: topAnchor.constraint(greaterThanOrEqualTo: view.topAnchor, constant: inset.top),
-                                          .bottom: bottomAnchor.constraint(greaterThanOrEqualTo: view.bottomAnchor, constant: inset.bottom),
-                                          .leading: leadingAnchor.constraint(greaterThanOrEqualTo: view.leadingAnchor, constant: inset.left),
-                                          .trailing: trailingAnchor.constraint(greaterThanOrEqualTo: view.trailingAnchor, constant: inset.right)]
-        defer { if activate { NSLayoutConstraint.activate(Array(constraint.values)) } }
-        return constraint
-    }
-    
-    @discardableResult
     public func alignCenterTo(view: UIView, activate: Bool = true) -> ConstraintInfo {
         translatesAutoresizingMaskIntoConstraints = false
         let constraint: ConstraintInfo = [.centerX: centerXAnchor.constraint(equalTo: view.centerXAnchor),
@@ -62,18 +40,6 @@ extension UIView {
     public func alignWithSuperView(inset: UIEdgeInsets = .zero, activate: Bool = true) -> ConstraintInfo {
         guard let superview = superview else { return [:] }
         return alignWith(view: superview, padding: inset, activate: activate)
-    }
-    
-    @discardableResult
-    public func alignLessThanOrEqualToWithSuperView(inset: UIEdgeInsets = .zero, activate: Bool = true) -> ConstraintInfo {
-        guard let superview = superview else { return [:] }
-        return alignLessThanOrEqualTo(superview, padding: inset, activate: activate)
-    }
-    
-    @discardableResult
-    public func alignGreaterThanOrEqualToWithSuperView(inset: UIEdgeInsets = .zero, activate: Bool = true) -> ConstraintInfo {
-        guard let superview = superview else { return [:] }
-        return alignGreaterThanOrEqualTo(superview, padding: inset, activate: activate)
     }
     
     @discardableResult
